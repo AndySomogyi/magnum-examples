ba2f1b912b1348cbc4ddde0b2dc2727426bbfc8b
Vladimír Vondruš <mosra@centrum.cz>
2016-10-08 03:30:54 +0200

    [wip] audio: ported to Emscripten.
    TODO: not able to create AL context :(

--- src/audio/AudioExample.cpp
+++ src/audio/AudioExample.cpp
@@ -58,6 +58,14 @@
 
 #include "configure.h"
 
+#ifdef MAGNUM_BUILD_STATIC
+/* Import plugins in static build */
+static int importStaticPlugins() {
+    CORRADE_PLUGIN_IMPORT(WavAudioImporter)
+    return 0;
+} CORRADE_AUTOMATIC_INITIALIZER(importStaticPlugins)
+#endif
+
 namespace Magnum { namespace Examples {
 
 using namespace Magnum::SceneGraph;
@@ -73,7 +81,7 @@ class AudioExample: public Platform::Application {
     private:
         void drawEvent() override;
 
-        void keyPressEvent(KeyEvent& event);
+        void keyPressEvent(KeyEvent& event) override;
 
         DebugTools::ResourceManager _manager;
 
@@ -169,7 +177,9 @@ AudioExample::AudioExample(const Arguments& arguments):
 
     /* Loop at 60 Hz max */
     setSwapInterval(1);
+    #ifndef CORRADE_TARGET_EMSCRIPTEN
     setMinimalLoopPeriod(16);
+    #endif
 }
 
 void AudioExample::drawEvent() {
